9eb019d00d8babd9b3cfda0549097c9d21c8196b
dream <80534443+kkkrupenich@users.noreply.github.com>
2021-05-30 21:40:42 +0300

    Add files via upload

---
+++ lab7/test.cpp
@@ -0,0 +1,243 @@
+#include <iostream>
+#include <assert.h>
+#include "pawnshop.h"
+#include <vector>
+#include <fstream>
+
+using namespace std;
+
+Pawnshop p;
+
+void testInput()
+{
+    p.input();
+    assert(p.itemList.size() == 6);
+    assert(p.itemList[2].itemName == "Banana");
+    assert(p.humanList.size() == 6);
+    assert(p.humanList[5].passport == 51234);
+}
+
+void testCheck()
+{
+    ifstream file("test.txt");
+    unsigned int a;
+    if(file.is_open())
+    {
+        while(!file.eof())
+        {
+            p.check(&a, &file);
+            assert((a < 0) == false);
+        }
+    }
+    file.close();
+    ofstream file2("test.txt");
+    if(file2.is_open())
+        file2 << "iphone " << 1200 << ' ' << 1300 << ' ' << 700 << endl << "amazing " << 0 << ' ' << 413 << ' ' << 123;
+    file.close();
+}
+
+void testAddToItemList()
+{
+    ifstream file("test.txt");
+    if(file.is_open())
+        while(!file.eof())
+            p.addToItemList(&file);
+
+    assert(p.itemList[6].itemName == "iphone");
+    assert(p.itemList[7].moneyToHuman == 123);
+    file.close();
+
+    ofstream file2("test.txt");
+    if(file2.is_open())
+        file2.clear();
+    file2 << "Leo " << "Galante " << 1234123 << ' ' << "amazing " << "morkovka " << 0 << ' ' << 123;
+    file.close();
+}
+
+void testAddToHumanList()
+{
+    ifstream file("test.txt");
+    if(file.is_open())
+        while(!file.eof())
+            p.addToHumanList(&file);
+    assert(p.humanList[6].firstname == "Leo");
+    assert(p.humanList[7].passport == 123);
+    file.close();
+
+    ofstream file2("test.txt");
+    if(file2.is_open())
+        file.clear();
+    file2 << 1 << ' ' << 1 << ' ' << 2 << ' ' << 1200 << ' ' << 2 << ' ' << 4 << ' ' << 123321;
+    file.close();
+}
+
+void testChangeInfo()
+{
+    ifstream file("test.txt");
+    if(file.is_open())
+        while(!file.eof())
+            p.changeInfo(&file);
+
+    assert(p.itemList[1].moneyToHuman == 1200);
+    assert(p.humanList[4].passport == 123321);
+    file.close();
+
+    ofstream file2("test.txt");
+    if(file2.is_open())
+        file.clear();
+    file2 << 1 << ' ' << 0 << ' ' << ' ' << 1 << ' ' << 2 << ' ' << 2 << ' ' << 1 << ' ' << 2 << ' ' << 2;
+    file.close();
+}
+
+void testDeleteInfo()
+{
+    ifstream file("test.txt");
+    if(file.is_open())
+        while(!file.eof())
+            p.deleteSmth(&file);
+
+    assert(p.itemList.size() == 4);
+    assert(p.humanList.size() == 4);
+    file.close();
+
+    ofstream file2("test.txt");
+    if(file2.is_open())
+        file2.clear();
+    file2.close();
+}
+
+void testShowAll()
+{
+    ofstream file("test.txt");
+    if(file.is_open())
+        p.showAll(&file);
+    file.close();
+
+    ifstream file2("test.txt");
+    if(file2.is_open())
+    {
+        string temp;
+        file2 >> temp;
+        assert(temp == "Item");
+        file2 >> temp;
+        assert(temp == "list:");
+        while(!file2.eof())
+            file2 >> temp;
+        assert(temp == "Empty");
+    }
+    file2.close();
+
+    file.open("test.txt");
+    if(file.is_open())
+        file.clear();
+    file.close();
+}
+
+void testCheckPrice()
+{
+    ofstream file("test.txt");
+    if(file.is_open())
+        p.checkPrice(&file);
+    file.close();
+
+    ifstream file2("test.txt");
+    if(file2.is_open())
+    {
+        string temp;
+        file2 >> temp;
+        assert(temp == "Assessed");
+        while(!file2.eof())
+            file2 >> temp;
+        assert(temp == "$13624");
+    }
+    file2.close();
+
+    file.open("test.txt");
+    if(file.is_open())
+        file.clear();
+    file << 2 << ' ' << 3;
+    file.close();
+}
+
+void testTakeSmth()
+{
+    ifstream file("test.txt");
+    if(file.is_open())
+        while(!file.eof())
+            p.takeSmth(&file);
+
+    assert(p.itemList[3].humanID == 123321);
+    assert(p.humanList[2].history == "Afroamerican");
+    file.close();
+
+    ofstream file2("test.txt");
+    if(file2.is_open())
+        file2.clear();
+    file2 << 2 << ' ' << 3;
+    file2.close();
+}
+
+void testReturnSmth()
+{
+    ifstream file("test.txt");
+    if(file.is_open())
+        while(!file.eof())
+            p.returnSmth(&file);
+    
+    assert(p.itemList[3].humanID == 0);
+    assert(p.humanList[2].history == "Empty");
+    file.close();
+
+    ofstream file2("test.txt");
+    if(file2.is_open())
+        file2.clear();
+    file2 << 2123;
+    file2.close();
+}
+
+void testSaveInfo()
+{
+    p.saveInfo();
+    ifstream file("newItems.txt");
+    if(file.is_open())
+    {
+        string temp;
+        file >> temp;
+        assert(temp == "Orange");
+        file >> temp;
+        assert(temp == "0");
+        while(!file.eof())
+            file >> temp;
+        assert(temp == "10");
+    }
+    file.close();
+    file.open("newPeople.txt");
+    if(file.is_open())
+    {
+        string temp;
+        file >> temp;
+        assert(temp == "Kir");
+        file >> temp;
+        assert(temp == "Kursedov");
+        while(!file.eof())
+            file >> temp;
+        assert(temp == "Empty");
+    }
+    file.close();
+}
+
+int main()
+{
+    testInput();
+    testCheck();
+    testAddToItemList();
+    testAddToHumanList();
+    testChangeInfo();
+    testDeleteInfo();
+    testShowAll();
+    testCheckPrice();
+    testTakeSmth();
+    testReturnSmth();
+    testSaveInfo();
+    return 0;
+}
